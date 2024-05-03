a7e2fd9d8115cc2ccf3d66f6341853b11faba96c
Javier Cohuo <al066180@uacam.mx>
2024-05-03 13:51:17 -0600

    ITS_8vo_Moviles_Practica4_66180

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,75 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-            title: const Text("Welcome to Flutter")
-        ),
-        body: const Center(
-          child: RandomWords(), // Utilizamos RandomWords en lugar de Text
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/words': (context) => const WordsScreen(),
+        '/nouns': (context) => const NounsScreen(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Welcome to Flutter")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/words');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                elevation: 5,
+              ),
+              child: const Text('Words'),
+            ),
+            const SizedBox(height: 30), // Separador
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/nouns');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                elevation: 5,
+              ),
+              child: const Text('Nouns'),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class WordsScreen extends StatelessWidget {
+  const WordsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Words")),
+      body: RandomWords(),
+    );
+  }
+}
+
+class NounsScreen extends StatelessWidget {
+  const NounsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Nouns")),
+      body: RandomNouns(),
     );
   }
 }
@@ -32,6 +93,47 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomNouns extends StatefulWidget {
+  const RandomNouns({Key? key}) : super(key: key);
+
+  @override
+  RandomNounsState createState() => RandomNounsState();
+}
+
+class RandomNounsState extends State<RandomNouns> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
